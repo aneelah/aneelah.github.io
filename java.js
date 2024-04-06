583d061a399a1f2b554e21fdc293840f2c418957
aneelah <155907011+aneelah@users.noreply.github.com>
2024-04-05 21:28:31 -0400

    Update java.js

---
+++ java.js
@@ -0,0 +1,20 @@
+var $select= $("<select></select>");
+$ ("mainmenu").append($select);
+$("#mainmenu a").each(function() {
+
+var $option=$("<option>");
+$option.val($(this).attr("href"));
+$option.text($(this).text());
+if($(this).parent().hasClass("selected")) {
+    $option.prop("selected", true);
+}
+$select.append($option);
+
+});
+function validateForm() {
+  let x = document.forms["contact"]["FirstName"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
